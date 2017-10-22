cb79aa302fecb08dbf4beba6f86d9e1af0a5be47
Anastasios Kalesis <32904730+peritheatrou@users.noreply.github.com>
2017-10-22 21:33:27 +0300

    Create config.php

---
+++ admin/config.php
@@ -0,0 +1,47 @@
+
+<?php
+
+ 
+
+/****  set database path 
+better rename (.database\) to another name ****/
+
+     $database_path="database/";
+////////////////////////////////////////////////
+
+
+/************  base path to save post *******/
+
+          $base_path='../image/';
+
+////////////////////////////////////////////// 
+
+/************** date format ***************/
+    $date_format = 'd-m-y H:i:s';
+
+///////////////////////////////////////////
+
+/************ numbers of users************/
+
+    $make_users=12;
+
+/********** numbers of category *********/
+    
+    $make_cat=12;
+
+///////////////////////////////////////////
+
+
+/*************** max numbers photo -> post ********/
+      
+          $max_photo=5;
+          $max_photo_size=5000000; // max upload size image per byte
+          $new_width_photo=600; //  test this ...Μικραινει το μέγεθος των φώτο... ισως να μην σας αρεσει. 
+
+/****** include functions for  avakas dbase   ****/
+
+ 
+
+
+?>
+
